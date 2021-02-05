5effb08f2e2b650fa839f3adb7377e26555e145b
julesfieux <julesfieux@yahoo.fr>
2021-02-05 11:38:42 +0100

    on s'en fou

--- test.c
+++ test.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
-/*char	*ft_tohexa(unsigned long long adress)
-{
-
-}*/
+#include <stdlib.h>
 
 int main()
 {
 	//char str[] = "WESH gangster !";
 	//printf("|%6.4p|\n", &str);
-	int nb = 42;
-	long int pointer = (long int)&nb;
-	printf("hexa = %p\n", &nb);
-	printf("decimal = %ld\n", pointer);
+
+	/*int nb = 0;
+	char *arg;
+
+	ft_treat_int(nb, &arg);
+	printf("arg = %s\n", arg);*/
 }
